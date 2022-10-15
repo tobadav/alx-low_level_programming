2fcb6fc6b827997bf509cdcd7a8b5dbac67b7965
tobadav <tobadav@gmail.com>
2022-10-15 15:16:38 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,8 @@
 #include<stdio.h>
-
+/* using puts function */
 int main(void)
 {
-	puts(0);
+	/* still put function */
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
 }
