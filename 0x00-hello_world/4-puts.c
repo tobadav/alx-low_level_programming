4fea167032281fa7370e7750b9b7366d73aad2c2
tobadav <tobadav@gmail.com>
2022-10-16 12:56:40 -0700

    put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,11 +2,11 @@
 /**
  * main - outputs main.c.
  * Description: 'removes the printf and replaces with puts.'
- * Return: Always success
+ * Return: 0
  */
 int main(void)
 
 {
-	puts("\"programming is like building a multilingual puzzle\n");
+	puts("\"programming is like building a multilingual puzzle");
 	return (0);
 }
