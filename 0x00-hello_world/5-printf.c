b3db615812428ba502f6855db4a2e45371dd27ce
tobadav <tobadav@gmail.com>
2022-10-16 14:01:06 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main - outputs to main.c.
+ * Description: 'removes the puts and replaces with printf'
+ * Return: 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
