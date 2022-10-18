d77e8bf129330b5e189425d8376977e499abf2ed
tobadav <tobadav@gmail.com>
2022-10-18 05:07:54 -0700

    alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - starting point
+ * Description: 'to print alphabet'
+ * Return: 0
+ */
+int main(void)
+{
+	char i;
+
+	i = "a"
+	while
+		(i <= "z") {
+			putchar(i);
+			i++;
+		}
+	putchar("\n");
+	return (0);
