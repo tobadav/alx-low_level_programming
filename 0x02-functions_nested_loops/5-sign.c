8f8641a0d9278d8d8114962abbf0b06b5318706b
tobadav <tobadav@gmail.com>
2022-10-19 05:32:32 -0700

    number sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_sign -> number sign are printed based on condition set
+ * @n: used as an argument
+ * Return: based on 0 1 -1
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('_');
+		return (-1);
+	}
+}
