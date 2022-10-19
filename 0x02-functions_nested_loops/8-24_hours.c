b115f370259a4acc4f5a41f1cb7ab7c41a26a63f
tobadav <tobadav@gmail.com>
2022-10-19 10:32:17 -0700

    prints minute of the day

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,17 +10,14 @@ void jack_bauer(void)
 
 	for (j = 0; j <= 23; j++)
 	{
-
-	for (b = 0; b <= 59; b++)
-	{
-
-	_putchar (b / 10 + '0');
-	_putchar (j % 10 + '0');
-	_putchar (':');
-	_putchar (b / 10 + '0');
-	_putchar (b % 10 + '0');
-	_putchar ('\n');
-
-	}
+		for (b = 0; b <= 59; b++)
+		{
+			_putchar((j / 10) + '0');
+			_putchar((j % 10) + '0');
+			_putchar(':');
+			_putchar((b / 10) + '0');
+			_putchar((b % 10) + '0');
+			_putchar('\n');
+		}
 	}
 }
