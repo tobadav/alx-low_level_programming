d283c4bc150b45593e1b8393d3db57e053358651
tobadav <tobadav@gmail.com>
2022-10-20 13:26:53 -0700

    prime factors of number

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - starting point
+ * Return: 0
+ */
+int main(void)
+{
+	unsigned long int x, y = 612852475143;
+
+	for (x = 3; x < 782849; x = x + 2)
+	{
+		while ((y % x == 0) && (y != i))
+			y = y / x;
+	}
+	printf("%lu\n", y);
+	return (0);
+}
