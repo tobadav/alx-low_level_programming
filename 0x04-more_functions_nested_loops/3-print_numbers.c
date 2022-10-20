7a040c1d1763c396fc18a42f93c2182b5679728a
tobadav <tobadav@gmail.com>
2022-10-20 04:48:40 -0700

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_numbers - print numbers from 0 - 9
+ * Return: void
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+	_putchar (i);
+	}
+	_putchar ('\n');
+}
