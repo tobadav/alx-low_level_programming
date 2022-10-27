c25d5181dc019176041adedccab9af1eb8a31ea3
tobadav <tobadav@gmail.com>
2022-10-27 09:20:08 -0700

    leet

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * leet - a function that encodes a string into 1337
+ * @s: string to encode
+ *
+ * Return: address of s
+ */
+char *leet(char *s)
+{
+	int x, y;
+
+	char a[] = "aAeEoOtTlL";
+	char b[] = "4433007711";
+
+	for (x = 0; *(s + x); x++)
+	{
+	for (y = 0; y <= 9; y++)
+	{
+	if (a[y] == s[x])
+	s[x] = b[y];
+	}
+	}
+	return (s);
+}
