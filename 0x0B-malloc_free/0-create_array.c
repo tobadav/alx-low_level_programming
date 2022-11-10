2bf7084637685a074756d80347ae6a1de40c1039
tobadav <tobadav@gmail.com>
2022-11-10 05:48:03 -0800

    array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,16 +11,14 @@
  */
 char *create_array(unsigned int size, char c)
 {
-	char *array;
+	char str;
 	unsigned int i;
 
-	if (size == 0)
-	return (NULL);
-	array = (char *)malloc(sizeof(char) * size);
-	if (array == NULL)
+	str = malloc(sizeof(char) * size);
+
+	if (size == 0 || str == NULL)
 	return (NULL);
 	for (i = 0; i < size; i++)
-	array[i] = c;
+	str[i] = c;
 
-	return (array);
-}
+	return (str);
