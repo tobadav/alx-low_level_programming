7874e7b7171375a2428f31e7ac2a91602c84e337
tobadav <tobadav@gmail.com>
2022-11-30 06:22:07 -0800

    delete node

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -17,22 +17,18 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 
 	if (copy == NULL)
 	return (-1);
-
 	if (index == 0)
 	{
 	*head = (*head)->next;
 	free(copy);
 	return (1);
 	}
-
 	for (node = 0; node < (index - 1); node++)
 	{
 	if (copy->next == NULL)
 	return (-1);
-
 	copy = copy->next;
 	}
-
 	tmp = copy->next;
 	copy->next = tmp->next;
 	free(tmp);
